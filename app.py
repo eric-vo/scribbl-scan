a508a50662fe834b64369af324f850e944c82089
Eric Vo <eric.aq.vo@gmail.com>
2023-08-09 00:02:53 -0700

    Edit app

--- app.py
+++ app.py
@@ -3,7 +3,5 @@ from flask import Flask
 from routes import home_bp
 
 
-def create_app():
-    app = Flask(__name__, instance_relative_config=False)
-    app.register_blueprint(home_bp)
-    return app
+app = Flask(__name__, instance_relative_config=False)
+app.register_blueprint(home_bp)
