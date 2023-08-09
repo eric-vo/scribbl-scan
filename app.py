a074dfae4a75710dd098c153814a52e8055b3d88
Eric Vo <eric.aq.vo@gmail.com>
2023-08-09 00:26:51 -0700

    Fix run

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ from flask import Flask
 from routes import home_bp
 
 
-app = Flask(__name__, instance_relative_config=False)
-app.register_blueprint(home_bp)
+def create_app():
+    app = Flask(__name__, instance_relative_config=False)
+    app.register_blueprint(home_bp)
