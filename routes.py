96ee6d83646f69eac0c2eddcabea39065374f33d
ewyzzia <giikae127@gmail.com>
2023-08-11 00:01:59 -0700

    Increase contrast on images, new easy.png

--- routes.py
+++ routes.py
@@ -1,17 +1,18 @@
 import base64
 import io
-import torch
 import json
-import cv2 # Sorry I'm mixing PIL and OpenCV in the same project :(
-import numpy as np
-import matplotlib.pyplot as plt
 
+import cv2  # Sorry I'm mixing PIL and OpenCV in the same project :(
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
 from flask import Blueprint, render_template, request
 from PIL import Image, ImageOps
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 print(device)
+
 
 def show_image_with_horizontal_lines(image, line_heights):
     # Read the image
@@ -23,10 +24,11 @@ def show_image_with_horizontal_lines(image, line_heights):
 
     # Add horizontal lines at specified heights
     for height in line_heights:
-        plt.axhline(y=height, color='r', linewidth=2)
+        plt.axhline(y=height, color="r", linewidth=2)
 
-    plt.title('Image with Horizontal Lines')
+    plt.title("Image with Horizontal Lines")
     plt.show()
+
 
 def std_dev(arr):
     if len(arr) == 0:
@@ -34,22 +36,41 @@ def std_dev(arr):
     mean = np.mean(arr)
     squared_diff = np.abs(arr - mean)
     variance = np.mean(squared_diff)
-    std_dev = variance #np.sqrt(variance)
+    std_dev = variance  # np.sqrt(variance)
     return std_dev
 
+
 LINE_SEGMENTATION_SCALE_FACTOR = 0.1
+
+
 def line_segmentation(image):
     og_image = image
-    image = cv2.resize(image, (int(og_image.shape[1] * LINE_SEGMENTATION_SCALE_FACTOR), int(og_image.shape[0] * LINE_SEGMENTATION_SCALE_FACTOR)), interpolation=cv2.INTER_AREA)
+    image = cv2.resize(
+        image,
+        (
+            int(og_image.shape[1] * LINE_SEGMENTATION_SCALE_FACTOR),
+            int(og_image.shape[0] * LINE_SEGMENTATION_SCALE_FACTOR),
+        ),
+        interpolation=cv2.INTER_AREA,
+    )
     brightness_array = np.mean(image, axis=1)
-    threshold = np.median(brightness_array) * 0.2 + np.average(brightness_array) * 0.8
+    threshold = (
+        np.median(brightness_array) * 0.2 + np.average(brightness_array) * 0.8
+    )
     binarized_array = np.where(brightness_array >= threshold, 1, 0).tolist()
     changes_indices = []
-    for i in range(1, len(binarized_array)): # tried doing a smart numpy thing here and it didn't work for some reason so uhhhh dumb solution ig
+    for i in range(1, len(binarized_array)):
+        # tried doing a smart numpy thing here and it didn't work
+        # for some reason so uhhhh dumb solution ig
         if binarized_array[i] != binarized_array[i - 1]:
             changes_indices.append(i)
 
-    spacings = np.array([changes_indices[i] - (0 if i == 0 else changes_indices[i - 1]) for i in range(0, len(changes_indices)) ])
+    spacings = np.array(
+        [
+            changes_indices[i] - (0 if i == 0 else changes_indices[i - 1])
+            for i in range(0, len(changes_indices))
+        ]
+    )
     new_changes_indices = []
     if len(changes_indices) > 2:
         lastHeight = changes_indices[0]
@@ -62,16 +83,21 @@ def line_segmentation(image):
             space = curHeight - lastHeight
             section_crop = image[lastHeight:curHeight, :]
             if not space < np.mean(spacings) * 0.4:
-                if (std_dev(section_crop) < std_dev(image) * 0.7 and i != 1) :
-                    new_changes_indices[-1] = curHeight * 0.35 + lastHeight * 0.65           
+                if std_dev(section_crop) < std_dev(image) * 0.7 and i != 1 and len(new_changes_indices) > 1:
+                    new_changes_indices[-1] = (
+                        curHeight * 0.35 + lastHeight * 0.65
+                    )
                 else:
                     new_changes_indices.append(curHeight)
             lastHeight = curHeight
 
     changes_indices = new_changes_indices
-    changes_indices = np.rint(np.array(changes_indices) / LINE_SEGMENTATION_SCALE_FACTOR)
-    #show_image_with_horizontal_lines(og_image, changes_indices)
+    changes_indices = np.rint(
+        np.array(changes_indices) / LINE_SEGMENTATION_SCALE_FACTOR
+    )
+    # show_image_with_horizontal_lines(og_image, changes_indices)
     return changes_indices.tolist()
+
 
 # Load model
 processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
@@ -98,6 +124,7 @@ def index():
         "index.html", data=login, title="ScribblScan", subtitle="ScribblScan"
     )
 
+
 def infer_on_img(image):
     pixel_values = processor(image, return_tensors="pt").pixel_values
     pixel_values = pixel_values.to(device)
@@ -106,6 +133,7 @@ def infer_on_img(image):
         generated_ids, skip_special_tokens=True
     )[0]
     return generated_text
+
 
 @home_bp.route("/demo", methods=["GET", "POST"])
 def demo():
@@ -119,10 +147,14 @@ def demo():
         img_arr = np.array(image)
         threshold = np.median(img_arr) * 0.2 + np.average(img_arr) * 0.8
         binarized_array = np.where(img_arr >= threshold, 1, 0)
+
         if np.mean(binarized_array) < 0.5:
             image = ImageOps.invert(image)
             print("Inverted image")
+        threshold = np.median(img_arr) * 0.2 + np.average(img_arr) * 0.8
+        contrast_applied_array = (np.exp( (img_arr - threshold) * -5 * (1/255) ) + 1)**-1
 
+        image = Image.fromarray((contrast_applied_array * 255).astype(np.uint8))
 
         # Get line segmentation (ewww I use opencv here ewwww)
         cv2_image = np.array(image.convert("L"))
@@ -132,11 +164,12 @@ def demo():
         out_text = ""
         last_crop_height = 0
         for i, height in enumerate(split_points):
-            cropped_image = image.crop((0, int(last_crop_height), image.width, int(height)));
+            cropped_image = image.crop(
+                (0, int(last_crop_height), image.width, int(height))
+            )
             out_text += infer_on_img(cropped_image) + "\n"
             last_crop_height = height
 
-        
         # Return text
         return json.dumps({"text": out_text})
 
